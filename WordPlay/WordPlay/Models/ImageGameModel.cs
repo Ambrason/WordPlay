6d7ffa61b94a39f9aa5f8f1341331cf5dd6434b7
Aron <rorran4@hotmail.com>
2015-09-23 10:31:29 +0200

    added imagegame model and controller

---
+++ WordPlay/WordPlay/Models/ImageGameModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WordPlay.Models
+{
+    public class ImageGameModel
+    {
+        public string Word { get; set; }
+        public int Point { get; set; }
+    }
+}
