6c44d4c3dd62877ad3e281181d18ee1e5c30c7c1
Aron <rorran4@hotmail.com>
2015-09-23 11:50:54 +0200

    extra

--- WordPlay/WordPlay/Controllers/ImageGameController.cs
+++ WordPlay/WordPlay/Controllers/ImageGameController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WordPlay.Repositories;
 
 namespace WordPlay.Controllers
 {
     public class ImageGameController : Controller
     {
+
+        //call on ImageGameRepository
+        private ImageGameRepository repo = new ImageGameRepository();
+
+
         // GET: ImageGame
         public ActionResult Index()
         {
