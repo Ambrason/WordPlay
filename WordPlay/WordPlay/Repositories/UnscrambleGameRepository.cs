8dd1c6cfa8e322cbc66170ed63da675b9b3312e7
Ambrason <ambrason@gmail.com>
2015-09-23 10:44:37 +0200

    UnscrambleGame controller + repository

---
+++ WordPlay/WordPlay/Repositories/UnscrambleGameRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WordPlay.Models;
+
+namespace WordPlay.Repositories
+{
+    public class UnscrambleGameRepository
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ICollection<UnscrambledSentence> GetItems()
+        {
+            
+        }
+    }
+}
