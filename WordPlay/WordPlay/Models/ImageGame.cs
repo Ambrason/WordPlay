441134903027e66019ab05e28a781da36392d839
Aron <rorran4@hotmail.com>
2015-09-23 14:47:50 +0200

    test

--- WordPlay/WordPlay/Models/ImageGame.cs
+++ WordPlay/WordPlay/Models/ImageGame.cs
@@ -9,7 +9,7 @@ namespace WordPlay.Models
     public class ImageGame
     {
         [Key]
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Image { get; set; }
         public string Word { get; set; }
         public int Point { get; set; }
