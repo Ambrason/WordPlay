4de17c5b85f2d45adfd388d89eb1d004f7806e7e
Aron <rorran4@hotmail.com>
2015-09-23 14:31:21 +0200

    test

--- WordPlay/WordPlay/Models/ImageGame.cs
+++ WordPlay/WordPlay/Models/ImageGame.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace WordPlay.Models
 {
-    public class ImageGameModel
+    public class ImageGame
     {
+        [Key]
+        public int ID { get; set; }
+        public string Image { get; set; }
         public string Word { get; set; }
         public int Point { get; set; }
     }
--- WordPlay/WordPlay/Repositories/ImageGameRepository.cs
+++ WordPlay/WordPlay/Repositories/ImageGameRepository.cs
@@ -1,46 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Web;
-using System.Web.UI;
 
 namespace WordPlay.Repositories
 {
     public class ImageGameRepository
     {
-        public void button1_Click(object sender, EventArgs e)
-        {
-            Image[] images = new Image[5];
-            images[0] = Image.FromFile(@"C:\Users\elev\Source\Repos\WordPlay\WordPlay\WordPlay\Images\ImageGame\airplane.jpg");
-            images[1] = Image.FromFile(@"C:\Users\elev\Source\Repos\WordPlay\WordPlay\WordPlay\Images\ImageGame\bear.jpg");
-            images[2] = Image.FromFile(@"C:\Users\elev\Source\Repos\WordPlay\WordPlay\WordPlay\Images\ImageGame\bicycle.jpg");
-            images[3] = Image.FromFile(@"C:\Users\elev\Source\Repos\WordPlay\WordPlay\WordPlay\Images\ImageGame\car.jpg");
-            images[4] = Image.FromFile(@"C:\Users\elev\Source\Repos\WordPlay\WordPlay\WordPlay\Images\ImageGame\boat.jpg");
-
-
-            string[] strNames = new String[5];
-            strNames[0] = "airplane";
-            strNames[1] = "bear";
-            strNames[2] = "bicycle";
-            strNames[3] = "car";
-            strNames[4] = "boat";
-
-
-            string[] strAnswers = new String[5];
-            //strAnswers[0] = "";
-            //strAnswers[1] = "";
-            //strAnswers[2] = "";
-            //strAnswers[3] = "";
-            //strAnswers[4] = "";
-
-
-
-            for (int i = 1; i <= 5; i++){
-                if (strAnswers[i] == strNames[i]){
-                    //om input är rätt
-                }
-            }
-        }
     }
 }
