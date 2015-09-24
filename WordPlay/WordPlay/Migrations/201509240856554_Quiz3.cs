e44d04f7cafa8613c148776c55770a5c3efe83d5
jarolex <jan.roos.lexicon@gmail.com>
2015-09-24 13:54:38 +0200

    Revert "Igen"
    This reverts commit bb3f2c449d2138d6fa9930a4303b3d30fb64506d.

--- WordPlay/WordPlay/Migrations/201509240856554_Quiz3.cs
+++ WordPlay/WordPlay/Migrations/201509240856554_Quiz3.cs
@@ -3,20 +3,16 @@ namespace WordPlay.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class arrggg : DbMigration
+    public partial class Quiz3 : DbMigration
     {
         public override void Up()
         {
-            AddColumn("dbo.PgTasks", "PgTaskAnswer", c => c.String());
             AlterColumn("dbo.QuizHighscores", "Name", c => c.String());
-            DropColumn("dbo.PgTasks", "PbTaskAnswer");
         }
         
         public override void Down()
         {
-            AddColumn("dbo.PgTasks", "PbTaskAnswer", c => c.String());
             AlterColumn("dbo.QuizHighscores", "Name", c => c.Int(nullable: false));
-            DropColumn("dbo.PgTasks", "PgTaskAnswer");
         }
     }
 }
